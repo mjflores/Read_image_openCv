5e97561535674003e15a300c5249f229785d87d6
Marco Flores <floresmj2@hotmail.com>
2017-08-03 14:25:54 -0500

    Agregue por error todo

---
+++ CMakeFiles/LeerImagen.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/LeerImagen.dir/programa1.cpp.o"
+  "LeerImagen.pdb"
+  "LeerImagen"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/LeerImagen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
